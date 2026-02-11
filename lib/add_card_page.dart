e3aff0d49b31776b3ca77a79e2c24b195d2d613f
fptra28 <faturrahman86.fr@gmail.com>
2026-02-11 13:31:27 +0700

    first commit

---
+++ lib/add_card_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'card_form_page.dart';
+
+class AddCardPage extends StatelessWidget {
+  const AddCardPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const CardFormPage();
+  }
+}
